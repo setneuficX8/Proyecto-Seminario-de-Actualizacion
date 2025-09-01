554f5352ab2ad4f3007ffb8c0ac6ac5978b88e94
setneuficX8 <andressetneufic@gmail.com>
2025-08-31 22:23:18 -0500

    esqueleto del componente

---
+++ src/components/landing.jsx
@@ -0,0 +1,27 @@
+export default function Landing(){
+    return (
+        <section>
+            <header>
+                <h1>BIENVENIDO A NUESTRO PROYECTO</h1>
+            </header>
+            <section>
+                <main>
+                <p>Esta es la página de inicio de nuestro proyecto, para su elaboración, participan:</p>
+                <ul>
+                    <li>Carlos Andres Cifuentes Montaño</li>
+                        <img src="" alt="" />
+                    <li>Darío Restrepo Landazury</li>
+                        <img src="" alt="" />
+                    <li>Key Dayana Arboleda Mina</li>
+                        <img src="" alt="" />
+                    <li>Jose Fernando Sinisterra Ibargüen</li>
+                        <img src="" alt="" />
+                </ul>
+            </main>
+            </section>
+            <footer>
+                {/* Aquí agregan el footer */}
+            </footer>
+        </section>
+    )
+}
