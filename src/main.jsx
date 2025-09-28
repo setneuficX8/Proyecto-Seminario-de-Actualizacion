ef7a840febbdd6c3b256c122e173f4decef46f46
DariusClover <dario503030@gmail.com>
2025-09-28 01:00:03 -0500

    Configurndo el supabase

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import App2 from './App2.jsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App/>  
+      <App2 />
     </Provider>
   </StrictMode>,
 )
